f76f56c1277022e8682a7c43ab0e8e740b5a3eb0
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2022-12-15 09:38:15 +0100

    Commit 1 Isma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitolLogoComponent } from './titol-logo/titol-logo.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { CistellaComponent } from './cistella/cistella.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitolLogoComponent,
+    MenuComponent,
+    FooterComponent,
+    CistellaComponent
   ],
   imports: [
     BrowserModule
