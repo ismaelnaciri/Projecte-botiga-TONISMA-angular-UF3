c2043c8fe633aaee27fae82526d72ae3e1d0d763
toni-padilla <98542721+toni-padilla@users.noreply.github.com>
2022-12-16 12:49:08 +0100

    dgr4e3

---
+++ src/app/cataleg/cataleg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cataleg',
+  templateUrl: './cataleg.component.html',
+  styleUrls: ['./cataleg.component.css']
+})
+export class CatalegComponent {
+
+}
