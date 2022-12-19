c82eba7c89722cbb0e657dfc32800a66b5c1ab0d
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2022-12-19 10:41:54 +0100

    Commit almost all links Isma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from "@angular/router";
 import { CistellaComponent } from './cistella/cistella.component'
 import { RegistreANDLoginComponent } from './registre-andlogin/registre-andlogin.component'
+import { CatalegComponent } from "./cataleg/cataleg.component";
 
 
 const routes: Routes = [
   {path: 'cistella', component: CistellaComponent},
-  {path: 'signin', component: RegistreANDLoginComponent}
+  {path: 'signin', component: RegistreANDLoginComponent},
+  {path: 'cataleg', component: CatalegComponent}
 
 ]
 
