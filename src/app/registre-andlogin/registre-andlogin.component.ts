995144aa78efb8e1cdf308b39c164f730be0d903
diazdani <daniel.diaz@institutvidreres.cat>
2023-03-27 19:30:55 +0200

    Commit prefireauth

--- src/app/registre-andlogin/registre-andlogin.component.ts
+++ src/app/registre-andlogin/registre-andlogin.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpParams} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {UsersService} from "../users.service";
+
+// @ts-ignore
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-registre-andlogin',
@@ -13,9 +18,63 @@ export class RegistreANDLoginComponent implements OnInit{
   nom:any;
   contrasenya:any;
   correu:any;
+  autenticat = this.serveiUsuari.autenticat;
+  nomAutenticat: any;
 
+  //Login
+  [x: string]: any;
+  email:any;
+  password:any;
+  correuTrobat: any;
 
-  constructor() {
+/*
+  async autenticar() {
+    let errorMessage = ' ';
+
+    await this.firebaseAuth.signInWithEmailAndPassword(this.email, this.password)
+      .then(res => {
+        this.serveiUsuari.autenticat = false;
+        this.serveiUsuari.usuari = JSON.stringify(res.user);
+        this.serveiUsuari.emailAutenticat = this.email;
+        this.correuTrobat = false;
+
+        for (let i = 0; i < this.serveiUsuari.arrClients.clients.length; i++) {
+          if (this.serveiUsuari.arrClients.clients[i].Correu == this.email) {
+            this.serveiUsuari.posAutenticat = i;
+            this.correuTrobat = true;
+          }
+        }
+        if (!this.correuTrobat) {
+          alert("Sembla que no disposem de les dades d'aquest client!");
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        errorMessage = error.message;
+      })
+    if (!this.serveiUsuari.autenticat) {
+      alert("Entrada denegada! \n" + errorMessage);
+    }
+  }
+*/
+  Logout(){
+    this.serveiUsuari.autenticat = false;
+    this.autenticat= false;
+    this.nomAutenticat= 'null';
+  }
+
+  registrar()
+  {
+    this.http.post<any>('http://localhost:3080/registrar', {
+      Correu: this.correu,
+      Nom: this.nom,
+      Contrasenya:this.contrasenya
+    }).subscribe();
+
+  }
+
+  constructor(private http:HttpClient,public router:Router, private serveiUsuari: UsersService, public firebaseAuth: AngularFireAuth) {
+
   }
 
   ngOnInit() {
@@ -43,3 +102,7 @@ export class RegistreANDLoginComponent implements OnInit{
     }*/
   }
 }
+function registrar() {
+    throw new Error('Function not implemented.');
+}
+
---
+++ src/app/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  public autenticat= false;
+  public posAutenticat = -1;
+  public emailAutenticat: any;
+  public arrClients: any;
+  public usuari: any;
+  constructor(private http:HttpClient) {
+    this.http.get<any>('http://localhost:3080/api/firebase').subscribe((document)=>{
+      this.arrClients = document;
+    });
+  }
+}
